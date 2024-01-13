dc42977b2b7446af198b87a333507b1eb4280e23
Sharif Akram <rifkit@gmail.com>
2024-01-13 12:01:41 -0500

    code samples from Ch6

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,29 @@
+﻿// Program that prompts the user for their grade
+
+while (true)
+{
+    Console.Write("Enter your grade (type 'exit' to quit): ");
+    string userGrade = Console.ReadLine();
+
+    if (userGrade.ToLower() == "exit")
+    {
+        break;
+    }
+
+    switch (userGrade.ToUpper())
+    {
+        case "A+":
+        case "A":
+            Console.WriteLine("Distinction");
+            break;
+        case "B":
+            Console.WriteLine("B Grade");
+            break;
+        case "C":
+            Console.WriteLine("C Grade");
+            break;
+        default:
+            Console.WriteLine("Fail");
+            break;
+    }
+}
